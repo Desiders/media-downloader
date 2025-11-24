20e75b4403b92af4d35d8c9f0d9876c8dd310794
Desiders <47452083+Desiders@users.noreply.github.com>
2025-11-25 01:53:55 +0300

    Add DI container init function

---
+++ worker/src/di_container.rs
@@ -0,0 +1,21 @@
+use froodi::{DefaultScope::App, async_impl::Container, async_registry, instance, registry};
+
+use crate::config::{Config, Version};
+
+pub fn init(config: Config, version: Version) -> Container {
+    let sync_registry = registry! {
+        scope(App) [
+            provide(instance(config.logging)),
+            provide(instance(config.limits)),
+            provide(instance(config.yt_dlp)),
+            provide(instance(config.yt_toolkit)),
+            provide(instance(config.yt_pot_provider)),
+            provide(instance(version)),
+        ]
+    };
+    let registry = async_registry! {
+        extend(sync_registry)
+    };
+
+    Container::new(registry)
+}
