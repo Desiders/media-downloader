5041981a73acd7732e3b42ee316fd9b6222f058f
Desiders <47452083+Desiders@users.noreply.github.com>
2025-11-25 03:34:08 +0300

    Combine thumbnail fields in `Video`

--- worker/src/entities/media.rs
+++ worker/src/entities/media.rs
@@ -6,49 +6,29 @@ use crate::{
 use serde::Deserialize;
 use std::path::PathBuf;
 use tempfile::TempDir;
-use url::{Host, Url};
-
-#[derive(Debug, Clone, Deserialize)]
-pub struct Thumbnail {
-    pub url: Option<String>,
-}
+use url::Host;
 
 #[allow(clippy::module_name_repetitions)]
 #[derive(Debug, Clone, Deserialize)]
 pub struct Video {
     pub id: String,
-    pub thumbnail: Option<String>,
-    pub thumbnails: Option<Vec<Thumbnail>>,
+    pub thumbnails: Vec<String>,
     pub original_url: String,
     pub width: Option<i64>,
     pub height: Option<i64>,
 }
 
 impl Video {
+    #[must_use]
     pub fn thumbnail_urls<'a>(&'a self, service_host: Option<&Host<&str>>) -> Vec<String> {
         let aspect_ratio = calculate_aspect_ratio(self.width, self.height);
         let aspect_kind = AspectKind::get_nearest(aspect_ratio);
-        let mut thumbnail_urls = get_urls_by_aspect(service_host, &self.id, aspect_kind);
 
-        if let Some(url) = &self.thumbnail {
-            thumbnail_urls.push(url.clone());
+        let mut urls = get_urls_by_aspect(service_host, &self.id, aspect_kind);
+        for url in self.thumbnails.iter() {
+            urls.push(url.clone());
         }
-        for Thumbnail { url } in self.thumbnails.as_deref().unwrap_or_default() {
-            if let Some(url) = url.clone() {
-                thumbnail_urls.push(url);
-            }
-        }
-        thumbnail_urls
-    }
-
-    pub fn domain(&self) -> Option<String> {
-        match Url::parse(&self.original_url) {
-            Ok(url) => match url.domain() {
-                Some(domain) => Some(domain.to_owned()),
-                None => None,
-            },
-            Err(_) => None,
-        }
+        urls
     }
 }
 
